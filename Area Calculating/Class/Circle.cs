f2ed2f377bee16ee866b0e690d148c6908672257
Максим Загорский <zagorskiymv@yandex.ru>
2022-04-10 20:37:48 +0300

    Создаие класса Circle

---
+++ Area Calculating/Class/Circle.cs
@@ -0,0 +1,46 @@
+﻿using Area_Calculating.Interface;
+using System;
+
+namespace Area_Calculating
+{
+    // Класс окружности
+    // Переменные класса сделаны с приватными set для защиты от искажения параметров фигуры.
+    // НЕ может быть круга, где радиус и диаметр отличаются.  
+    public class Cirсle : IShape
+    {
+        public double Diameter { get; private set; }
+        public double Radius { get; private set; }
+        public double Area { get; private set; }
+        public double Perimeter { get; private set; }
+
+        // Конструктор класса Circle. Задается обязательный параметр радиуса и вычисляется диаметр для дальнейшего вычисления площади
+        public Cirсle(double radius)
+        {
+            Radius = radius;
+            Diameter = 2 * radius;
+        }
+
+        //Вычисление площади коружности по формуле π⋅r^2​​
+        public double CalculateArea()
+        {
+            Area = Math.PI * Math.Pow(Radius, 2);
+            return Area;
+        }
+
+        //Вычисление длины окружности(периметр) 
+        public double CalculatePerimeter()
+        {
+            Perimeter = 2 * Math.PI * Radius;
+            return Perimeter;
+        }
+
+        //Вычисление площади окружности через  диаметр. Формула: (π⋅D^2)/4
+        public double CalculateAreaByDiameter()
+        {
+            Area = (Math.PI * Math.Pow(Diameter, 2))/4;
+            return Perimeter;
+        }
+
+    }
+
+}
--- Area Calculating/Interface/IShape.cs
+++ Area Calculating/Interface/IShape.cs
@@ -6,9 +6,12 @@ using System.Threading.Tasks;
 
 namespace Area_Calculating.Interface
 {
+    // Интерфейс фигуры, у всех 2D фигур, вне зависимости от количества сторон есть площадь и периметр 
     interface IShape
     {
-        double Area();
-        double Perimeter();
+        double Area { get; }
+        double Perimeter { get; }
+        double CalculateArea();
+        double CalculatePerimeter();
     }
 }
