a93487fd2f246324b2ee87ac6a88960b93229aac
Максим Загорский <zagorskiymv@yandex.ru>
2022-04-11 10:14:20 +0300

    Добавление sql запроса

---
+++ Area Calculating/Task2_SQL.sql
@@ -0,0 +1,9 @@
+SELECT
+		p.product_name,
+    	c.category_name
+FROM  
+	db.correspondence_products_and_categories cpac
+LEFT OUTER JOIN 
+	db.products p ON  p.id = cpac.id
+LEFT OUTER JOIN 
+	db.categories c ON cpac.categories_id = c.id;
