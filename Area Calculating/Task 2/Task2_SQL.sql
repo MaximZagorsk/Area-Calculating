372ab2d425d5f1b5f079841ad88d4cd238e06339
MaximZagorsk <50720953+MaximZagorsk@users.noreply.github.com>
2022-04-12 20:57:16 +0300

    Update Task2_SQL.sql
    Fix exception

--- Area Calculating/Task 2/Task2_SQL.sql
+++ Area Calculating/Task 2/Task2_SQL.sql
@@ -4,6 +4,6 @@ SELECT
 FROM  
 	db.correspondence_products_and_categories cpac
 LEFT OUTER JOIN 
-	db.products p ON  p.id = cpac.id
+	db.products p ON  p.id = cpac.product_id
 LEFT OUTER JOIN 
 	db.categories c ON cpac.categories_id = c.id;
