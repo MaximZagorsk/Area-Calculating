6be1243c4b7af4d0b5dfceff32a9bf2fc1ae7678
MaximZagorsk <50720953+MaximZagorsk@users.noreply.github.com>
2022-04-12 20:59:13 +0300

    Update Task2_SQL.sql
    Fix mistake

--- Area Calculating/Task 2/Task2_SQL.sql
+++ Area Calculating/Task 2/Task2_SQL.sql
@@ -1,9 +1,9 @@
 SELECT
-		p.product_name,
+	p.product_name,
     	c.category_name
 FROM  
 	db.correspondence_products_and_categories cpac
 LEFT OUTER JOIN 
 	db.products p ON  p.id = cpac.product_id
 LEFT OUTER JOIN 
-	db.categories c ON cpac.categories_id = c.id;
+	db.categories c ON cpac.category_id = c.id;
